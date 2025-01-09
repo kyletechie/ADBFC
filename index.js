8ef60f203d64bb2a2c185148b5aeb703b8c0da36
Kairu-bit <kyletilano@gmail.com>
2025-01-09 16:41:58 +0800

    Add antiturnoffadb

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ const SUCCESS_COLOR = "\x1b[1;32m";
 const ERROR_COLOR = "\x1b[1;31m";
 const PAIRING_COLOR = "\x1b[1;34m";
 const CONNECTING_COLOR = PAIRING_COLOR;
+const screenBlockList = ["NotificationShade", "com.android.settings"];
 
 const port = process.argv[2];
 const pairPort = process.argv[3];
 const pairCode = process.argv[4];
 const killServer = process.argv[5] === "1";
+const antiturnoff = process.argv[6] || "1";
 
 if (!port || !pairPort || !pairCode) {
   console.log(`${INFO_COLOR}[INFO] ${SUCCESS_COLOR}Usage: npm start <port> <pairPort> <pairCode> <killServer: 0>
@@ -23,10 +25,10 @@ Arguments:
     - Useful for fixing the "failed to connect" issue. 
     - Avoid if multiple devices are connected.
     - Use if connecting only one device.
-    Values: 0 = false (default), 1 = true`);
+    Values: 0 = false (default), 1 = true
+  <antiturnoff>: Values: 1 = yes (default), 0 = no`);
   exit(0);
 }
-
 
 let ifconfigOutput = execSync("ifconfig", { stdio: "pipe", shell: true }).toString();
 
@@ -38,6 +40,23 @@ function run(cmd, opts = { stdio: "inherit", shell: true }) {
   catch (e) {
     console.log(`${ERROR_COLOR}[ERROR] ${SUCCESS_COLOR}Error running "${cmd}":`, e.message);
     process.exit(1);
+  }
+}
+
+function antiTurnOff(){
+  console.log(`${INFO_COLOR}[INFO] ${SUCCESS_COLOR}Android Debug Bridge (ATO) is Running...`);
+  while (true) {
+    const now = new Date().toLocaleString();
+    //const output = "mFocusedApp=Window{hskahj sishkssnsj sishsjss com.facebook.orca}"
+    const output = run(`adb shell dumpsys window`, { stdio: "pipe" });
+    const currentFocus = output.match(/(mCurrentFocus|mFocusedApp)=\S+{([^}]+)}/);
+    const currentScreen = currentFocus ? currentFocus[0] : run(`adb shell dumpsys statusbar`, { stdio: "pipe" })
+    screenBlockList.forEach((blockList) => {
+      if (currentScreen.includes(blockList)) {
+        run(`adb shell input keyevent HOME`);
+        console.log(`${INFO_COLOR}${now} [INFO] Blocking ${blockList}.`);
+      }
+    });
   }
 }
 
@@ -94,7 +113,7 @@ function getIPv4Devices() {
     let devices = [];
     console.log(`${INFO_COLOR}[INFO] ${SUCCESS_COLOR}Fetching Wlan0 IPv4 Address...`);
     const wlanIPv4 = getWlan0IPv4().trim();
-    const nmapScanOutput = execSync(`nmap -sn -n --min-parallelism 20 ${wlanIPv4}/24`, { stdio: "pipe", shell: true }).toString();
+    const nmapScanOutput = execSync(`nmap -sn -n ${wlanIPv4}/24`, { stdio: "pipe", shell: true }).toString();
     const scanReport = nmapScanOutput.match(/Nmap scan report for .*?\n/g) || [];
 
     if (scanReport.length === 0) {
@@ -136,6 +155,9 @@ async function main() {
     });
 
     await adbConnect(port, pairPort, pairCode, targetDevice);
+    if (antiturnoff === "1"){
+      antiTurnOff();
+    }
   }
   catch (e) {
     console.log(`${ERROR_COLOR}[ERROR] ${SUCCESS_COLOR}${e.message}`);
